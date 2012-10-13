2290964f394153c4220754149922f591daece71d
Gudao Luo <gudao.luo@gmail.com>
2012-10-14 00:31:19 +0200

    project anycities completed

--- anycities/app/models/city.rb
+++ anycities/app/models/city.rb
@@ -1,9 +1,3 @@
 class City < ActiveRecord::Base
   attr_accessible :name, :zip_code
-
-  # def fetch_name
-  #   url = "http://ws.geonames.org/postalCodeLookupJSON?postalcode=#{zip_code}&country=DE"
-  #   json = Net::HTTP.get_response(URI.parse(url)).body
-  #   update_attribute(:name, JSON.parse(json)["postalcodes"].first["placeName"])
-  # end
 end
